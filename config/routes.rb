1bf80fd024aef7961c77764a922c6625e946034d
Stevey Z <sjzerneri@gmail.com>
2015-07-04 19:00:36 -0400

    Added Heart Capability

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :posts
+  resources :hearts, only: :create
   get 'about' => 'pages#about'
 
   root 'pages#welcome'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704215255) do
+ActiveRecord::Schema.define(version: 20150704223130) do
+
+  create_table "hearts", force: :cascade do |t|
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
